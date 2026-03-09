66ef53df6a6392d2eabd66b40ccc62ae278ff952
akalweit <akalweit@wisc.edu>
2026-03-09 12:16:41 +0100

    adding back home details

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,12 @@
+<script setup>
+const modules = [
+  { title: 'Module 1', route: '/module/1' },
+  { title: 'Module 2', route: '/module/2' },
+  { title: 'Module 3', route: '/module/3' },
+  { title: 'Module 4', route: '/module/4' },
+]
+</script>
+
 <template>
   <div>Home is working</div>
 </template>
