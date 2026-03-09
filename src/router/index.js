db931f24eb2039981945ad4d3e7833294b115c1c
akalweit <akalweit@wisc.edu>
2026-03-09 12:23:44 +0100

    :9

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/views/Home.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
-  history: createWebHistory('/maffeodrinkslearn/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [{ path: '/', component: Home }],
 })
 
